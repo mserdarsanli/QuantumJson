cb1e537d2aabecb0a64005dec7a801bcdbea3a1d
Mustafa Serdar Sanli <mserdarsanli@gmail.com>
2016-11-12 23:03:12 +0000

    Add benchmark correctness checks for scenario3 except for staticjson

--- benchmark/scenario3/impl_boost_pt.cpp
+++ benchmark/scenario3/impl_boost_pt.cpp
@@ -43,6 +43,43 @@ void Benchmark(int repeat, const std::string &input)
 		pt::ptree root;
 		pt::read_json(ss, root);
 
+		#ifdef BENCHMARK_CHECK_CORRECTNESS
+
+		std::string user1_name, user1_image;
+		int user1_bronze;
+		std::string user30_name, user30_image;
+		int user30_bronze;
+
+		// boost_pt has a horrible api, not allowing random access to
+		// list elements.
+		int n = 0;
+		for (const auto &ch : root.get_child("items"))
+		{
+			++n;
+			if (n == 1)
+			{
+				user1_name = ch.second.get<std::string>("display_name");
+				user1_image = ch.second.get<std::string>("profile_image");
+				user1_bronze = ch.second.get<int>("badge_counts.bronze");
+			}
+			if (n == 30)
+			{
+				user30_name = ch.second.get<std::string>("display_name");
+				user30_image = ch.second.get<std::string>("profile_image");
+				user30_bronze = ch.second.get<int>("badge_counts.bronze");
+			}
+		}
+
+		// Check first and last values
+		CHECK(user1_name == "Jon Skeet");
+		CHECK(user1_image == "https://www.gravatar.com/avatar/6d8ebb117e8d83d74ea95fbdd0f87e13?s=128&d=identicon&r=PG");
+		CHECK(user1_bronze == 7502);
+		CHECK(user30_name == "Gumbo");
+		CHECK(user30_image == "https://www.gravatar.com/avatar/cd501083459cbc21fccae78e2d03bee2?s=128&d=identicon&r=PG");
+		CHECK(user30_bronze == 669);
+
+		#endif
+
 		BENCHMARK_LOOP_END;
 	}
 
--- benchmark/scenario3/impl_nlohmann_json.cpp
+++ benchmark/scenario3/impl_nlohmann_json.cpp
@@ -36,6 +36,25 @@ void Benchmark(int repeat, const std::string &input)
 
 		auto j3 = nlohmann::json::parse(input);
 
+		#ifdef BENCHMARK_CHECK_CORRECTNESS
+
+		std::string user1_name = j3["items"][0]["display_name"];
+		std::string user1_image = j3["items"][0]["profile_image"];
+		int user1_bronze = j3["items"][0]["badge_counts"]["bronze"];
+		std::string user30_name = j3["items"][29]["display_name"];
+		std::string user30_image = j3["items"][29]["profile_image"];
+		int user30_bronze = j3["items"][29]["badge_counts"]["bronze"];
+
+		// Check first and last values
+		CHECK(user1_name == "Jon Skeet");
+		CHECK(user1_image == "https://www.gravatar.com/avatar/6d8ebb117e8d83d74ea95fbdd0f87e13?s=128&d=identicon&r=PG");
+		CHECK(user1_bronze == 7502);
+		CHECK(user30_name == "Gumbo");
+		CHECK(user30_image == "https://www.gravatar.com/avatar/cd501083459cbc21fccae78e2d03bee2?s=128&d=identicon&r=PG");
+		CHECK(user30_bronze == 669);
+
+		#endif
+
 		BENCHMARK_LOOP_END;
 	}
 
