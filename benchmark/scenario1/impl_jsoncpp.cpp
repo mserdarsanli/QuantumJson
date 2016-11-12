6c7976283ecacf20b7396c7ea29ad8c1e19656ff
Mustafa Serdar Sanli <mserdarsanli@gmail.com>
2016-11-12 22:10:01 +0000

    Add benchmark correctness checks for scenario1

--- benchmark/scenario1/impl_jsoncpp.cpp
+++ benchmark/scenario1/impl_jsoncpp.cpp
@@ -36,6 +36,18 @@ void Benchmark(int repeat, const std::string &input)
 		Json::Reader reader;
 		reader.parse(input, root);
 
+		#ifdef BENCHMARK_CHECK_CORRECTNESS
+		std::string url1 = root["data"]["children"][0]["data"]["url"].asString();
+		std::string url25 = root["data"]["children"][24]["data"]["url"].asString();
+		int score1 = root["data"]["children"][0]["data"]["score"].asInt();
+		int score25 = root["data"]["children"][24]["data"]["score"].asInt();
+
+		CHECK(url1 == "http://i.imgur.com/RkeezA0.jpg");
+		CHECK(score1 == 6607);
+		CHECK(url25 == "https://www.youtube.com/watch?v=PMNFaAUs2mo");
+		CHECK(score25 == 4679);
+		#endif
+
 		BENCHMARK_LOOP_END;
 	}
 
